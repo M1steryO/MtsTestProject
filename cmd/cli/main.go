aa41d095220c17b9b300d6a95e935c20ffed236b
dmitrypeschansky <pda1205@gmail.com>
2026-03-12 23:56:05 +0300

    fix error misses

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -15,24 +15,26 @@ func main() {
 	flag.Parse()
 
 	if flag.NArg() != 1 {
-		fmt.Fprintf(os.Stderr, "usage: %s [--json] <git-repo-url>\n", filepath.Base(os.Args[0]))
+		_, _ = fmt.Fprintf(os.Stderr, "usage: %s [--json] <git-repo-url>\n", filepath.Base(os.Args[0]))
 		os.Exit(2)
 	}
 
 	service := app.NewService()
 	result, err := service.Run(context.Background(), flag.Arg(0))
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "error:", err)
+		_, _ = fmt.Fprintln(os.Stderr, "error:", err)
 		os.Exit(1)
 	}
 
 	if *jsonOutput {
 		if err := output.PrintJSON(os.Stdout, result); err != nil {
-			fmt.Fprintln(os.Stderr, "error:", err)
+			_, _ = fmt.Fprintln(os.Stderr, "error:", err)
 			os.Exit(1)
 		}
 		return
 	}
 
-	output.PrintText(os.Stdout, result)
+	if err = output.PrintText(os.Stdout, result); err != nil {
+		return
+	}
 }
